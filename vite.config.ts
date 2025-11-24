7316467bae66976b960b26520067d7cad409ab4d
avi1967 <avishamoghe@gmail.com>
2025-11-24 22:21:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: "/Team-Bharadwaj/",   // <-- IMPORTANT: change this!
+  //base: "/Team-Bharadwaj/",   
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
